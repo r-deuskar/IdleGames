45efc55f4dead5dd0f2d18b10be2a26f973db5ce
r-deuskar <164165106+r-deuskar@users.noreply.github.com>
2025-07-18 11:58:55 +0100

    Update generate_config.sh (Something seems to be going wrong)

--- generate_config.sh
+++ generate_config.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 # Reset config file
 > ./config.js
 # Find all the APPS_ environment variables in the environment
 declare -a customVars
+
 for key in $(env | awk -F "=" '{print $1}' | grep ".*APPS_.*")
 do
   customVars+=($key)
@@ -12,3 +13,4 @@ for key in "${customVars[@]}"
 do
   echo "window.$key='${!key}';" >> ./config.js
 done
+echo "APPS_SCRIPT_URL=$APPS_SCRIPT_URL"
